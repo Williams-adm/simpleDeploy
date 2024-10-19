8aedffd41fbf226e50fc451f8ef75da699df7476
WilliamsAdm <i2310607@continental.edu.pe>
2024-10-18 22:37:14 -0500

    pag actualizada

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,9 +10,9 @@
     <div class="text-center p-6 bg-white rounded-lg shadow-lg">
         <h1 class="text-3xl font-bold text-gray-800 mb-4">¡Bienvenido!</h1>
         <p class="text-xl text-gray-600 mb-6">Conexión exitosa</p>
-        <p class="text-lg text-gray-500">URL de mi servidor front: <span class="font-semibold text-blue-600">http://mi-servidor.com</span></p>
+        <p class="text-lg text-gray-500">URL de mi servidor front: <span class="font-semibold text-blue-600">https://customerdeploy.web.app</span></p>
         <div class="mt-4">
-            <a href="http://mi-servidor.com" target="_blank" class="text-blue-500 hover:underline">Visitar mi pagina web</a>
+            <a href="https://customerdeploy.web.app" target="_blank" class="text-blue-500 hover:underline">Visitar mi pagina web</a>
         </div>
     </div>
 </body>
